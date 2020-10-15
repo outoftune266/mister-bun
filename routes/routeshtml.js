6320b47c59575fff6eb2d562e9c94bea3b950d9e
Chad Tarpey <chadtarpey@gmail.com>
2020-10-14 19:43:34 -0500

    created paths

--- routes/routeshtml.js
+++ routes/routeshtml.js
@@ -1,14 +1,18 @@
-const { appendFile } = require("fs");
 var path = require("path");
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "home.html"));
-});
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "table.html"));
-});
+function initHtmlRoutes(app) {
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "home.html"));
+    });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "reservation.html"));
-});
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "table.html"));
+    });
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "reservation.html"));
+    });
+}
+
+module.exports = initHtmlRoutes;
