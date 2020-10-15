e1c089005f34ef396a84b5661edbf26b4a95af41
Zak Monnet <zakmonnet@gmail.com>
2020-10-14 19:24:19 -0500

    added html routes

--- routes/routeshtml.js
+++ routes/routeshtml.js
@@ -2,5 +2,13 @@ const { appendFile } = require("fs");
 var path = require("path");
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname))
-})
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "table.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+});
